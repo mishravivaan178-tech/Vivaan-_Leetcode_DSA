4212c4b0f5d2eec1b39960f919af126fe38993d3
Vivssssss <mishravivaan178@gmail.com>
2026-08-23 16:01:35 +0530

    Time: 64 ms (22.58%), Space: 61.8 MB (44.09%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void func(int ind, string s, vector<string>&temp, vector<vector<string>>&ans){
+        if(ind==s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=ind;i<s.size();i++){
+            if(pal(s,ind,i)){
+            temp.push_back(s.substr(ind, i - ind + 1));
+            func(i+1,s,temp,ans);
+            temp.pop_back();
+            }
+
+        }
+    }
+    bool pal(string s, int st, int e){
+        while(st<=e){
+            if(s[st++]!=s[e--]){
+                return false;
+            }
+    }
+     return true;
+        }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>temp;
+        func(0,s,temp,ans);
+        return ans;
+    }
+};
