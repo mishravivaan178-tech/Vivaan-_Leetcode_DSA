b32464838c1101213cea9803cfcc63df0ce4a66c
Vivssssss <mishravivaan178@gmail.com>
2026-08-24 14:53:44 +0530

    Time: 2 ms (73.18%), Space: 10 MB (99.76%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,60 @@
+class Solution {
+    public:
+    bool s(int row,int col, vector<string>&board, int n){
+        int dr=row;
+        int dc=col;
+
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q'){
+                return false;
+            }
+            row--;
+            col--;
+        }
+        row=dr;
+        col=dc;
+        while(col>=0){
+            if(board[row][col]=='Q'){
+                return false;
+            }
+            col--;
+        }
+        col=dc;
+        row=dr;
+        while(row<n && col>=0){
+            if(board[row][col]=='Q'){
+                return false;
+            }
+            row++;
+            col--;
+        }
+        return true;
+    }
+    public:
+    void solve(int col, vector<string>&board,vector<vector<string>>&ans, int n){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(s(row,col,board,n)){
+                board[row][col]='Q';
+                solve(col+1,board,ans,n);
+                board[row][col]='.';
+            }
+        }
+
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        solve(0,board,ans,n);
+        return ans;
+
+    }
+};
